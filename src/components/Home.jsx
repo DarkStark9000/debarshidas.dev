139118dd90d519802ad18e60611997a25f7aa1b6
Debarshi Das <debarshi8das@gmail.com>
2023-12-01 23:04:18 +0530

    🔄🚀journey section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,69 @@ const Home = () => {
         </motion.div>
       </motion.div>
 
-      <div className="about__container">I am Iron Man</div>
+      <div className="about__container">
+        <div className="about__content">
+          <div className="background__text">About</div>I just build stuff that I can think of (mostly) - cause other
+          times I'm limited by the technology of my time, but one day we will figure this out. <br /> <br /> In other
+          stories I main Killjoy, ironically that is exactly what kills my joy after a complete rank downgrade :(
+        </div>
+        <div className="about__image">
+          <img src="/plumage_1.jpg" alt="Debarshi's alt image" />
+        </div>
+      </div>
+
+      <div className="journey__container">
+        <div className="journey__content">
+          <div className="background__text">Journey</div>I did stuff - I lived. I'm still living. I'm still doing stuff.
+        </div>
+        <motion.div
+          className="journey__line"
+          drag
+          dragConstraints={{ top: 10, bottom: 5 }}
+          dragSnapToOrigin={true}
+          dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+        >
+          <motion.div
+            className="journey__event-dot"
+            drag
+            dragConstraints={{ left: 0, right: 0 }}
+            dragSnapToOrigin={true}
+            dragTransition={{ bounceStiffness: 900 }}
+          >
+            <span>Born</span>
+          </motion.div>
+
+          <motion.div
+            className="journey__event-dot"
+            drag
+            dragConstraints={{ left: 0, right: 0 }}
+            dragSnapToOrigin={true}
+            dragTransition={{ bounceStiffness: 900 }}
+          >
+            <span>went to school (masti only)</span>
+          </motion.div>
+
+          <motion.div
+            className="journey__event-dot"
+            drag
+            dragConstraints={{ left: 0, right: 0 }}
+            dragSnapToOrigin={true}
+            dragTransition={{ bounceStiffness: 900 }}
+          >
+            <span>went to college (moye moye)</span>
+          </motion.div>
+
+          <motion.div
+            className="journey__event-dot"
+            drag
+            dragConstraints={{ left: 0, right: 0 }}
+            dragSnapToOrigin={true}
+            dragTransition={{ bounceStiffness: 900 }}
+          >
+            <span>designing this page</span>
+          </motion.div>
+        </motion.div>
+      </div>
     </>
   );
 };
